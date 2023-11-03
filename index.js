2ffc4b64f830f473cad000a2ba67cdbb9423bddb
Amber Chen <amberchen@Ambers-MacBook-Pro-2.local>
2023-11-03 16:51:14 -0700

    Sockets Added

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 let express = require('express');
 let app = express();
 
-
 app.use(express.static('public'));
 
+let http = require('http');
+let server = http.createServer(app);
+
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("Server listening on localhost: ", port);
+server.listen(port, () => {
+    console.log("Server listening on localhost: " + port);
 });
+
+let io = require('socket.io');
+io = new io.Server(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected: ' + socket.id);
+
+    //Receive msgObj from client
+    socket.on('msg', (data) => {
+        console.log("Received a 'msg' event");
+
+        //Emit message to all clients
+        io.sockets.emit('msg', data);
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected: ' + socket.id);
+    })
+})
--- public/app.js
+++ public/app.js
@@ -1,17 +1,66 @@
+let msgInput = document.getElementById('msg-input');
+let chatFeed = document.getElementById('chat-feed');
+let sendButton = document.getElementById('send-button');
+let chatContainer = document.getElementById('chat-container');
+
 window.addEventListener('load', ()=>{
     console.log('loaded');
+    let socket = io();
 
+    socket.on('connect',() => {
+        console.log('client connected');
+    })
 
+    socket.on('msg', (data) => {
+        console.log("Message arrived!");
+        console.log(data);
 
+        //Receive message from server
+        //need to get text to pop up above sprite
+
+        //original appending code from socket example
+        // let msgEl = document.createElement('p');
+        // msgEl.innerHTML = data.msg;
+        // chatFeed.appendChild(msgEl);
+    })
+
+    
+
+    //'Enter' key to submit message
+    msgInput.addEventListener('keydown', function (e) { // 
+        if (e.key === 'Enter' || e.key === 13) {
+            sendButton.click();
+            console.log('clicked');
+        }
+    });
+
+    sendButton.addEventListener('click', () => {
+        sendMessage();
+    })
+
+    //
+    function sendMessage() {
+        let curMsg = msgInput.value;
+        let msgObj = {"msg": curMsg};
+
+        //Socket: send msgObj to server
+        socket.emit('msg', msgObj);
+
+        //Clear the message input field
+        msgInput.value = "";
+    }
 })
 
-
+//#Sources - HTML element coordinates: https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+let inputRect = chatContainer.getBoundingClientRect();
+console.log(inputRect.top, inputRect.right, inputRect.bottom, inputRect.left);
 
 let spriteDrawn=false;
 let coords = [];
 let x;
 let y;
 
+//#Source - sprites: https://creative-coding.decontextualize.com/making-games-with-p5-play/
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
@@ -34,8 +83,7 @@ function draw() {
     spr.attractionPoint(0.5, x, y);
   }
   let distance = dist(spr.position.x, spr.position.y, x, y);
-        if (distance < 5) {
-            // Stop the sprite when it's close to the mouse
+        if (distance < 5) { // Stop the sprite when it's close to the mouse
             spr.setSpeed(0);
             spriteDrawn = false;
         }
@@ -44,19 +92,26 @@ function draw() {
     // console.log("sprite drawn");
 }
 
+//#Source: https://stackoverflow.com/questions/7030229/storing-coordinates-in-array-in-javascript
 function storeCoordinate(x, y, array) {
     array.push(x);
     array.push(y);
 }
 
 function mouseClicked() {
-    spriteDrawn = true;
     storeCoordinate(mouseX, mouseY, coords);
     // to loop through coordinate values
     for (let i = 0; i < coords.length; i+=2) {
         x = coords[i];
         y = coords[i+1];
     } 
+    
+    //inputRect.top, inputRect.right, inputRect.bottom, inputRect.left
+    if(y>inputRect.bottom){
+        spriteDrawn = true;
+    }
+    
+    
 }
 
 
