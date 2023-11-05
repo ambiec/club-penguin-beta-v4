9f010460777293ae40928b738346d0728921b6b8
Amber Chen <amberchen@Ambers-MacBook-Pro-2.local>
2023-11-05 14:47:46 -0800

    first commit

--- index.js
+++ index.js
@@ -13,20 +13,43 @@ server.listen(port, () => {
 
 let io = require('socket.io');
 io = new io.Server(server);
+let connects = []
+let newClient;
 
 io.on('connection', (socket) => {
     console.log('Client connected: ' + socket.id);
 
-    //Receive msgObj from client
+    //Only emit if socket.id is new
+    if (!connects.includes(socket.id)) {
+        newClient = socket.id;
+        io.emit('newConnect', newClient);
+        connects.push(socket.id);
+    }
+
+    socket.on('existingSprites', (data) => {
+        io.emit('existingSprites', data);
+
+    })
+
+    //Sprite position data from client
+    socket.on('mouse',(data) => {
+        io.emit('mouse', data); //Emit position to all clients
+    })
+
+    //Message data from client
     socket.on('msg', (data) => {
         console.log("Received a 'msg' event");
-
-        //Emit message to all clients
-        io.emit('msg', data);
+        io.emit('msg', data); //Emit message to all clients
     })
 
 
     socket.on('disconnect', () => {
         console.log('Client disconnected: ' + socket.id);
+        
+
+
+        //Remove disconnected client from list #Source: https://sentry.io/answers/remove-specific-item-from-array/
+        let dcClient = socket.id
+        io.emit('clientSplice', dcClient);
     })
 })
--- public/app.js
+++ public/app.js
@@ -3,36 +3,139 @@ let chatFeed = document.getElementById('chat-feed');
 let sendButton = document.getElementById('send-button');
 let chatContainer = document.getElementById('chat-container');
 
+let spr =[];
+let sprObj = {};
+let spriteMove=false;
+
+let coords = [];
+let x;
+let y;
+let targetX;
+let targetY;
+let updated = false;
+
+let clickID;
 let msgData;
+let key;
 
-
-window.addEventListener('load', ()=>{
-    console.log('loaded');
-    let socket = io();
+let socket = io();
 
     socket.on('connect',() => {
         console.log('client connected');
     })
 
-    socket.on('msg', (data) => {
-        console.log("Message arrived!");
-        console.log(data);
-        
+    // Receiving 'newConnect' event from SERVER
+    socket.on('newConnect', (newClient)=> {
 
-        //Receive message from server
-        //need to get text to pop up above sprite
+        // Create sprite unique to newClient socket.id
+        spr[newClient] = createSprite(
+            width/2, height/2, 40, 40);
+        spr[newClient].shapeColor = color(0);
+        spr[newClient].rotateToDirection = true;
+        spr[newClient].maxSpeed = 4;
+        spr[newClient].friction = 0.1;
 
-        msgData = data.msg;
-        //I tried JSON.stringify, data.data, data.value lol
+        // Create sprite data object sprObj - different from spr array
+        key = newClient;
+        sprObj[key] = {
+            id: newClient,
+            sprX: spr[newClient].position.x,
+            sprY: spr[newClient].position.y,
+        }
 
-        //original appending code from socket example
-        // let msgEl = document.createElement('p');
-        // msgEl.innerHTML = data.msg;
-        // chatFeed.appendChild(msgEl);
+        // console.log(spr);
+        socket.emit('existingSprites', sprObj);
 
     })
 
-    
+    socket.on('existingSprites', (data) => {
+        for (let clientId in data) {
+            if (clientId != socket.id) {
+                
+                console.log(socket.id);
+                // console.log(data[clientId].sprX + ', '+ data[clientId].sprY);
+
+                if(!spr[clientId]) {
+                    let existingX = data[clientId].sprX;
+                    let existingY = data[clientId].sprY;
+
+                    console.log(existingX + ', ' + existingY);
+
+                    spr[clientId] = createSprite(
+                        existingX, existingY, 40, 40);
+                    spr[clientId].shapeColor = color(0);
+                    spr[clientId].rotateToDirection = true;
+                    spr[clientId].maxSpeed = 4;
+                    spr[clientId].friction = 0.1;
+                }
+                // } else {
+                //     let mousePos = {
+                //         id: clientId,
+                //         x: data[clientId].sprX,
+                //         y: data[clientId].sprY
+                //         }
+                
+                //         socket.emit('mouse', mousePos);
+                // }
+                
+            }
+        }
+    })
+
+
+    /*socket.on('clientSplice',(dcClient) => {
+        // console.log(sprites.clientId);
+        // console.log(dcClient);
+        for (let i = 0; i < sprites.length; i++) {
+            if (sprites[i].clientId === dcClient) {
+                sprites[i].sprite.remove();
+                sprites.splice(i, 1);
+                // console.log(sprites);
+            }
+        }
+    })*/
+
+    // Receiving 'mouse' event from SERVER
+    socket.on('mouse', (data) => {
+        targetX = data.x;
+        targetY = data.y;
+        clickID = data.id;
+
+        // console.log(clickID + ' clicked'); // which client clicked
+        
+        storeCoordinate(targetX, targetY, coords);
+
+        // Loop through coordinate values
+        for (let i = 0; i < coords.length; i+=2) {
+            x = coords[i];
+            y = coords[i+1];
+        } 
+        
+        // Keep sprite out of chat box area
+        if(y>inputRect.bottom + 30){
+            spriteMove = true;
+        }
+    })
+
+
+    socket.on('msg', (data) => {
+        console.log("Message arrived!");
+        // console.log(data);
+
+        //Receive message from server
+        //need to get text to pop up above sprite
+        msgData = data.msg;
+        //I tried JSON.stringify, data.data, data.value lol
+
+
+
+        /* Original code from socket example - append to HTML
+            let msgEl = document.createElement('p');
+            msgEl.innerHTML = data.msg;
+            chatFeed.appendChild(msgEl);*/
+
+    })
+
 
     //'Enter' key to submit message
     msgInput.addEventListener('keydown', function (e) { // 
@@ -57,27 +160,23 @@ window.addEventListener('load', ()=>{
         //Clear the message input field
         msgInput.value = "";
     }
-})
 
-//#Sources - HTML element coordinates: https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+//#Source - HTML element coordinates: https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
 let inputRect = chatContainer.getBoundingClientRect();
-console.log(inputRect.top, inputRect.right, inputRect.bottom, inputRect.left);
+// console.log(inputRect.top, inputRect.right, inputRect.bottom, inputRect.left);
 
-let spriteDrawn=false;
-let coords = [];
-let x;
-let y;
+
+
+
+/////////////////
+// P5 | P5Play //
+/////////////////
+let logOnce = false;
 
 //#Source - sprites: https://creative-coding.decontextualize.com/making-games-with-p5-play/
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
-    spr = createSprite(
-        width/2, height/2, 40, 40);
-    spr.shapeColor = color(255,0,0);
-    spr.rotateToDirection = true;
-    spr.maxSpeed = 4;
-    spr.friction = 0.1;
 
     //p5.play sprite
     //Step 1. Draw sprite
@@ -86,20 +185,40 @@ function setup() {
 
 function draw() {
     background(255);
-  if (spriteDrawn === true) {
-    console.log(x);
-    spr.attractionPoint(0.5, x, y);
-  }
-  let distance = dist(spr.position.x, spr.position.y, x, y);
-        if (distance < 5) { // Stop the sprite when it's close to the mouse
-            spr.setSpeed(0);
-            spriteDrawn = false;
+
+    // Match clickID to sprObj.id #Source: ChatGPT
+    for (let clientId in sprObj) {
+        if (sprObj.hasOwnProperty(clientId) && sprObj[clientId].id === clickID) {
+            // console.log(clickID + " exists in the object");
+
+            if (spriteMove === true) {
+                spr[clientId].attractionPoint(0.5, x, y);
+            }
+
+            let distance = dist(spr[clientId].position.x, spr[clientId].position.y, x, y);
+            if (distance < 5) { // Stop the sprite when it's close to the mouse
+                spr[clientId].setSpeed(0);
+
+                sprObj[clientId].sprX = spr[clientId].position.x;
+                sprObj[clientId].sprY = spr[clientId].position.y; 
+
+                if (spriteMove === true) {
+                    socket.emit('existingSprites', sprObj);
+                    console.log (sprObj);
+                    spriteMove = false;
+                }
+            }
         }
-  drawSprites();
-  printMessage();
+    }
+
     
-    // console.log("sprite drawn");
+  
+  drawSprites();
+//   printMessage();
 }
+
+
+
 
 //#Source: https://stackoverflow.com/questions/7030229/storing-coordinates-in-array-in-javascript
 function storeCoordinate(x, y, array) {
@@ -108,17 +227,14 @@ function storeCoordinate(x, y, array) {
 }
 
 function mouseClicked() {
-    storeCoordinate(mouseX, mouseY, coords);
-    // to loop through coordinate values
-    for (let i = 0; i < coords.length; i+=2) {
-        x = coords[i];
-        y = coords[i+1];
-    } 
-    
-    //inputRect.top, inputRect.right, inputRect.bottom, inputRect.left
-    if(y>inputRect.bottom){
-        spriteDrawn = true;
-    }
+    //send sprite position to server
+    let mousePos = {
+        id: socket.id,
+        x: mouseX,
+        y: mouseY
+        }
+
+        socket.emit('mouse', mousePos); 
 }
 
 function printMessage() {
